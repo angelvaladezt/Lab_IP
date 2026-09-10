b0bf2c0cfb7341f64924659a7d3119370c7a2154
Angel Valadez <angelvaladezt@gmail.com>
2026-09-10 17:43:01 -0600

    UDPATE UNIDAD2

--- UNIDAD2/03_for.py
+++ UNIDAD2/03_for.py
@@ -1,26 +1,22 @@
-# Trabajando con rangos con for
-for numero in range(1, 3, 2):
+for numero in range(1, 7): # Trabajando con rangos con for
     cuadrado = numero ** 2
     print(numero, cuadrado)
 
-# Trabajando con listas con for y enumerate
-materias = ["Python", "Linux", "Interfaces"]
+materias = ["Python", "Linux", "Interfaces"]  #Trabajando listas con for y enumerate
+
 for posicion, materia in enumerate(materias, start=1):
     print(f"{posicion}. {materia}")
 
-# Trabajando con listas con for
-materias = ["Python", "Linux", "Interfaces"]
 for materia in materias:
     print(materia)
 
-# Trabajando con cadenas con for
 cadena = "0123456789ABCDEF"
 for letra in cadena:
     print(letra)
 
-# Accediendo a una posición de la cadena
-print(cadena[15])
-
-# Trabajando con índices con for
-for i in range(0, 16):
+for i in range (len(cadena)):
     print(cadena[i])
+
+for numero in range (0,7,3):
+    cuadrado = numero**2
+    print(numero,cuadrado)
