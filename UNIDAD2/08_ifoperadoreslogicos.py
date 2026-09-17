256ec723eaf03f60d6e61dc594353c238c81e3fb
Angel Valadez <angelvaladezt@gmail.com>
2026-09-17 11:20:19 -0600

    CREATE 07, 08 operadores logicos

---
+++ UNIDAD2/08_ifoperadoreslogicos.py
@@ -0,0 +1,17 @@
+edad = 18
+tiene_credencial = True 
+tiene_adeudo = False
+
+if edad >= 18:
+    es_mayor=True
+else:
+    es_mayor=False
+
+documento_valido = tiene_credencial
+sin_adeudo = not tiene_adeudo
+if es_mayor and documento_valido and sin_adeudo:
+    autorizado = True
+else:
+    autorizado = False
+autorizado = es_mayor and documento_valido and sin_adeudo
+print(autorizado)
